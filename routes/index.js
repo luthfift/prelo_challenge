fc760e315e4391ba65491cfb46e2dd7900f1b4e5
luthfift <ml.fathur@gmail.com>
2017-09-21 20:12:34 +0700

    router post login and redirect to lovelist with token

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
 });
 
-/* GET lovelsit page. */
-router.get('/lovelist', function(req, res, next) {
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  request({
+  	method: 'post',
+  	url: "https://dev.prelo.id/api/auth/login",
+  	body: {
+  		username_or_email: req.body.username_or_email,
+  		password: req.body.password
+  	},
+  	json: true
+  }, function(error,response,body){
+  	if (!error && response.statusCode == 200){
+  		res.redirect('/lovelist/'+body._data.token);
+  	}else
+  		res.status(400);
+      	res.json({message: "Bad Request"});
+  })
+});
+
+/* GET lovelist page. */
+router.get('/lovelist/', function(req, res, next) {
   res.render('lovelist', { title: 'Lovelist' });
+});
+
+router.get('/lovelist/(:token)', function(req, res, next) {
+  res.render('lovelist', { title: 'Lovelist', token: req.params.token });
 });
 
 module.exports = router;
